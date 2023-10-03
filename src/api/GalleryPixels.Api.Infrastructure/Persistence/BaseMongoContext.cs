cb1b5276d51e94de5f2f7a6b0114f8e3e04637ee
Bram <bram.esendam@gmail.com>
2023-10-03 21:13:29 +0200

    Added Serilog & MongoDB connection

---
+++ src/api/GalleryPixels.Api.Infrastructure/Persistence/BaseMongoContext.cs
@@ -0,0 +1,25 @@
+﻿using GalleryPixels.Api.Infrastructure.Persistence.Configurations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace GalleryPixels.Api.Infrastructure.Persistence;
+
+public class BaseMongoContext
+{
+    /// <summary>
+    ///     Creates a new <see cref="BaseMongoContext" />.
+    /// </summary>
+    protected BaseMongoContext(IEnumerable<ICollectionConfigurator> collectionConfigurators)
+    {
+        foreach (var collectionConfigurator in collectionConfigurators)
+            collectionConfigurator.ConfigureCollection();
+
+        // Set up MongoDB conventions
+        var pack = new ConventionPack
+        {
+            new EnumRepresentationConvention(BsonType.String)
+        };
+
+        ConventionRegistry.Register("EnumStringConvention", pack, _ => true);
+    }
+}
