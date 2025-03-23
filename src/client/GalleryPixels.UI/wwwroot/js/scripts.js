69ce7fd921a9e85cbca3a8b4d32e6f3299f95177
BrammyS <bram.esendam@gmail.com>
2025-03-23 10:09:02 +0100

    Removed test logs

--- src/client/GalleryPixels.UI/wwwroot/js/scripts.js
+++ src/client/GalleryPixels.UI/wwwroot/js/scripts.js
@@ -23,10 +23,8 @@ function updateDarkModeSetting() {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.documentElement.classList.add('dark')
-        console.log("Added dark mode");
     } else {
         document.documentElement.classList.remove('dark')
-        console.log("Removed dark mode");
     }
 }
 
