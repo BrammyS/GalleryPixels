02aace1f2efe39a70a1a5a5b524957f8976c5ba4
BrammyS <bram.esendam@gmail.com>
2025-09-01 11:26:43 +0200

    Fix: Failing build

--- src/api/GalleryPixels.Api.Application/Common/Pipelines/PerformancePipelineBehaviour.cs
+++ src/api/GalleryPixels.Api.Application/Common/Pipelines/PerformancePipelineBehaviour.cs
@@ -4,28 +4,17 @@ using Microsoft.Extensions.Logging;
 
 namespace GalleryPixels.Api.Application.Common.Pipelines;
 
-public class PerformancePipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+public class PerformancePipelineBehaviour<TRequest, TResponse>(ILogger<PerformancePipelineBehaviour<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    private readonly ILogger<PerformancePipelineBehaviour<TRequest, TResponse>> _logger;
-
-    public PerformancePipelineBehaviour(ILogger<PerformancePipelineBehaviour<TRequest, TResponse>> logger)
-    {
-        _logger = logger;
-    }
-
-    public async ValueTask<TResponse> Handle(
-        TRequest request,
-        CancellationToken cancellationToken,
-        MessageHandlerDelegate<TRequest, TResponse> next
-    )
+    public async ValueTask<TResponse> Handle(TRequest request, MessageHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken)
     {
         var startTime = Stopwatch.GetTimestamp();
         var response = await next(request, cancellationToken).ConfigureAwait(false);
         var elapsedTime = Stopwatch.GetElapsedTime(startTime);
 
         if (elapsedTime.TotalMilliseconds > 1500)
-            _logger.LogWarning(
+            logger.LogWarning(
                 "Color-Chan.Api Long Running Request: {Name} ({ElapsedTime} milliseconds) {Request}",
                 typeof(TRequest).Name,
                 elapsedTime.TotalMilliseconds.ToString("F"),
--- src/api/GalleryPixels.Api.Application/Common/Pipelines/ValidationPipelineBehaviour.cs
+++ src/api/GalleryPixels.Api.Application/Common/Pipelines/ValidationPipelineBehaviour.cs
@@ -4,26 +4,15 @@ using ValidationException = GalleryPixels.Api.Domain.Exceptions.ValidationExcept
 
 namespace GalleryPixels.Api.Application.Common.Pipelines;
 
-public class ValidationPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+public class ValidationPipelineBehaviour<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
 {
-    private readonly IEnumerable<IValidator<TRequest>> _validators;
-
-    public ValidationPipelineBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    public async ValueTask<TResponse> Handle(TRequest request, MessageHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken)
     {
-        _validators = validators;
-    }
-
-    public async ValueTask<TResponse> Handle(
-        TRequest request,
-        CancellationToken cancellationToken,
-        MessageHandlerDelegate<TRequest, TResponse> next
-    )
-    {
-        if (!_validators.Any()) return await next(request, cancellationToken).ConfigureAwait(false);
+        if (!validators.Any()) return await next(request, cancellationToken).ConfigureAwait(false);
 
         var context = new ValidationContext<TRequest>(request);
-        var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)))
+        var validationResults = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)))
             .ConfigureAwait(false);
         var failures = validationResults.SelectMany(r => r.Errors).Where(f => f is not null).ToList();
 
